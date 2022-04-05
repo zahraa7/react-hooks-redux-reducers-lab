7bb5862a710f7146b77836c76e26fcfef96d77fc
zahraa7 <zahraa.knass@hotmail.com>
2022-04-05 16:23:01 +0300

    done

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,29 @@
+const initialState = { friends: [] };
+
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {
+        friends: [
+          ...state.friends,
+          {
+            name: action.payload.name,
+            hometown: action.payload.hometown,
+            id: action.payload.id,
+          },
+        ],
+      };
+
+    case "friends/remove":
+      return {
+        ...state,
+        friends: state.friends.filter((user) => {
+          if (user.id === action.payload.id) {
+            return false;
+          }
+        }),
+      };
+    default:
+      return state;
+  }
 }
